9f5038732c18e28083cde34230189413da0104c5
Vamshi S <149765018+vamshiis@users.noreply.github.com>
2025-03-29 13:09:09 +0000

    Data-Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let name="vamshi"
+let age=18
+let isLoggedIn=true
+
+// Data types in JavaScript number=>2 to power 53 
+// bigInt
+// string => " "
+//  boolean => true/false
+//  null => standalone value
+// undefines => no value
+// symbol =>=> used for unique
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
